176367d19f3192b42a0a1fe5bf98c4d489a71351
Vishnu Vardhan Reddy Naini <vishnu@vishnunaini.com>
2017-01-01 08:43:30 +0530

    surnia: rebrand to LineageOS
    Change-Id: I7cc36bd7bf368c70863b1bb0ccdd470eef6bf61d

--- lineage.mk
+++ lineage.mk
@@ -13,11 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Inherit from surnia device
-$(call inherit-product, device/motorola/surnia/device.mk)
-
-# Device identifier. This must come after all inclusions
-$(call inherit-product-if-exists, vendor/motorola/surnia/surnia-vendor.mk)
+$(call inherit-product, device/motorola/surnia/full_surnia.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 540
